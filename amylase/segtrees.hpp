d837b9b5f49baa5f4f70b9959569c2880271382c
amylase <amylase.inquiry@gmail.com>
2020-10-07 00:04:55 +0900

    fixed segtrees.

--- amylase/segtrees.hpp
+++ amylase/segtrees.hpp
@@ -16,14 +16,14 @@ namespace amylase {
 
     // segtrees: `prod`_segtree
     template<class T> using min_segtree = atcoder::segtree<T, min<T>, std::numeric_limits<T>::max>;
-    template<class T> using max_segtree = atcoder::segtree<T, max<T>, std::numeric_limits<T>::min>;
+    template<class T> using max_segtree = atcoder::segtree<T, max<T>, std::numeric_limits<T>::lowest>;
     template<class T> using sum_segtree = atcoder::segtree<T, add<T>, zero<T>>;
 
     // lazy segtrees: `prod`_`func`_segtree
     template<class T> using min_min_segtree = atcoder::lazy_segtree<T, min<T>, std::numeric_limits<T>::max, T, min<T>, min<T>, std::numeric_limits<T>::max>;
     template<class T> using min_sum_segtree = atcoder::lazy_segtree<T, min<T>, std::numeric_limits<T>::max, T, add<T>, add<T>, zero<T>>;
-    template<class T> using max_max_segtree = atcoder::lazy_segtree<T, max<T>, std::numeric_limits<T>::min, T, max<T>, max<T>, std::numeric_limits<T>::min>;
-    template<class T> using max_sum_segtree = atcoder::lazy_segtree<T, max<T>, std::numeric_limits<T>::min, T, add<T>, add<T>, zero<T>>;
+    template<class T> using max_max_segtree = atcoder::lazy_segtree<T, max<T>, std::numeric_limits<T>::lowest, T, max<T>, max<T>, std::numeric_limits<T>::lowest>;
+    template<class T> using max_sum_segtree = atcoder::lazy_segtree<T, max<T>, std::numeric_limits<T>::lowest, T, add<T>, add<T>, zero<T>>;
     template<class T> using sum_sum_segtree = atcoder::lazy_segtree<T, add<T>, zero<T>, T, add<T>, add<T>, zero<T>>;
 
 }  // namespace amylase

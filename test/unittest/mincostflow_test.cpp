6d83427526118f46f8de21eebe4f867aed86dd05
Kohei Morita <yosupo06@gmail.com>
2020-10-18 04:15:23 +0900

    add stress test for mincostflow

--- test/unittest/mincostflow_test.cpp
+++ test/unittest/mincostflow_test.cpp
@@ -1,7 +1,10 @@
+#include <atcoder/maxflow>
 #include <atcoder/mincostflow>
+
 #include <numeric>
 #include <tuple>
 #include <vector>
+#include "../utils/random.hpp"
 
 #include <gtest/gtest.h>
 
@@ -66,7 +69,7 @@ TEST(MincostflowTest, Assign) {
 
 TEST(MincostflowTest, OutOfRange) {
     mcf_graph<int, int> g(10);
-    
+
     EXPECT_DEATH(g.slope(-1, 3), ".*");
     EXPECT_DEATH(g.slope(3, 3), ".*");
 }
@@ -94,4 +97,70 @@ TEST(MincostflowTest, Invalid) {
     // https://github.com/atcoder/ac-library/issues/51
     EXPECT_DEATH(g.add_edge(0, 0, -1, 0), ".*");
     EXPECT_DEATH(g.add_edge(0, 0, 0, -1), ".*");
+}
+
+TEST(MincostflowTest, Stress) {
+    for (int phase = 0; phase < 1000; phase++) {
+        int n = randint(2, 20);
+        int m = randint(1, 100);
+        int s, t;
+        std::tie(s, t) = randpair(0, n - 1);
+        if (randbool()) std::swap(s, t);
+
+        mf_graph<int> g_mf(n);
+        mcf_graph<int, int> g(n);
+        for (int i = 0; i < m; i++) {
+            int u = randint(0, n - 1);
+            int v = randint(0, n - 1);
+            int cap = randint(0, 10);
+            int cost = randint(0, 10000);
+            g.add_edge(u, v, cap, cost);
+            g_mf.add_edge(u, v, cap);
+        }
+        int flow, cost;
+        std::tie(flow, cost) = g.flow(s, t);
+        ASSERT_EQ(g_mf.flow(s, t), flow);
+
+        int cost2 = 0;
+        std::vector<int> v_cap(n);
+        for (auto e : g.edges()) {
+            v_cap[e.from] -= e.flow;
+            v_cap[e.to] += e.flow;
+            cost2 += e.flow * e.cost;
+        }
+        ASSERT_EQ(cost, cost2);
+
+        for (int i = 0; i < n; i++) {
+            if (i == s) {
+                ASSERT_EQ(-flow, v_cap[i]);
+            } else if (i == t) {
+                ASSERT_EQ(flow, v_cap[i]);
+            } else {
+                ASSERT_EQ(0, v_cap[i]);
+            }
+        }
+
+        // check: there is no negative-cycle
+        std::vector<int> dist(n);
+        while (true) {
+            bool update = false;
+            for (auto e : g.edges()) {
+                if (e.flow < e.cap) {
+                    int ndist = dist[e.from] + e.cost;
+                    if (ndist < dist[e.to]) {
+                        update = true;
+                        dist[e.to] = ndist;
+                    }
+                }
+                if (e.flow) {
+                    int ndist = dist[e.to] - e.cost;
+                    if (ndist < dist[e.from]) {
+                        update = true;
+                        dist[e.from] = ndist;
+                    }
+                }
+            }
+            if (!update) break;
+        }
+    }
 }

d497dc3cf3e866a47f52b4104b75cb33eb7f4eba
xiaodao <lychees67@gmail.com>
2021-07-01 03:03:01 +0800

    Add KMP

--- atcoder/string.hpp
+++ atcoder/string.hpp
@@ -270,6 +270,31 @@ std::vector<int> z_algorithm(const std::string& s) {
     return z_algorithm(s2);
 }
 
+// Reference:
+// D. E. Knuth, J. H. Morris, V. R. Pratt
+// Fast pattern matching in strings
+template <class T> std::vector<int> kmp(const std::vector<T>& s) {
+    int n = int(s.size());
+    if (n == 0) return {};
+    std::vector<int> pi(n);
+    pi[0] = -1;
+    for (int i = 1, j = -1; i < n; i++) {
+        while (j >= 0 && s[i] != s[j+1]) j = pi[j];
+        if (s[i] == s[j+1]) ++j;
+        pi[i] = j;
+    }
+    return pi;
+}
+
+std::vector<int> kmp(const std::string& s) {
+    int n = int(s.size());
+    std::vector<int> s2(n);
+    for (int i = 0; i < n; i++) {
+        s2[i] = s[i];
+    }
+    return kmp(s2);
+}
+
 }  // namespace atcoder
 
 #endif  // ATCODER_STRING_HPP
